e5dfa3ec1f48e3ba922d5b664e6d72e0a392f2b8
Raksha567 <rakshasawade2015@gmail.com>
2024-08-09 17:23:46 +0530

    Add files via upload

---
+++ AreaOfParallelogram.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+	float b,h;
+	float area;
+	printf("Enter base and height:");
+	scanf("%f%f",&b,&h);
+	area=b*h;
+	printf("Area of Parallelogram is %f",area);
+	getch();
+}
