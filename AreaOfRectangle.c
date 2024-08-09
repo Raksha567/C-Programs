ec004eb3873b9443c63332fbdeaf43131326ebdd
Raksha567 <rakshasawade2015@gmail.com>
2024-08-09 17:24:12 +0530

    Add files via upload

---
+++ AreaOfRectangle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+	int l,area,b;
+	printf("Enter length and breadth:");
+	scanf("%d%d",&l,&b);
+	area=l*b;
+	printf("Area of rectangle is %d",area);
+	getch();
+}
