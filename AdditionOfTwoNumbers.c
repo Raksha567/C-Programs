1562b27b7cb607141c1c26c6d9e929b4e2170f5b
Raksha567 <rakshasawade2015@gmail.com>
2024-08-09 17:21:55 +0530

    Add files via upload

---
+++ AdditionOfTwoNumbers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+	int num1,num2,result;
+	printf("Enter two numbers");
+	scanf("%d%d",&num1,&num2);
+	result=num1+num2;
+	printf("Addition is %d",result);
+	getch();
+}
