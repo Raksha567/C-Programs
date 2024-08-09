f0c25c42cfd6a654685d4b5cc00eed93a46af59c
Raksha567 <rakshasawade2015@gmail.com>
2024-08-09 17:43:11 +0530

    Add files via upload

---
+++ DisplayIntegerEquivalentOfLetters.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(){
+	char ch;
+	printf("Ascii Value of Lowercase Letters:\n");   
+	for(ch='a';ch<='z';ch++){
+	printf("Ascii value of %c is %d\n",ch,ch);
+}
+printf("Ascii value of Upper case letters:\n");
+for(ch='A';ch<='Z';ch++){
+	printf("Ascii value of %c is %d\n",ch,ch);
+}
+	
+	
+}
