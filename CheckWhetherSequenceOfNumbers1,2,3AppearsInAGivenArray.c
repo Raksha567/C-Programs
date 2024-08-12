a9c424ae67eb9161dea8d8f2c235c728f93abd8c
Raksha567 <rakshasawade2015@gmail.com>
2024-08-12 12:43:37 +0530

    Add files via upload

---
+++ CheckWhetherSequenceOfNumbers1,2,3AppearsInAGivenArray.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main(){
+	int a[]={1,4,4,1,2,3,6,7,8};
+	int n=sizeof(a)/sizeof(a[0]);
+	int i;
+	for( i=0;i<n;i++){
+		if(a[i]==1&&a[i+1]==2&&a[i+2]==3){
+			printf("True sequence of 1,2,3 is present in an array.");
+			
+			
+		
+		}
+
+		
+	}
+		
+		}
+		
+	
+	
+	
