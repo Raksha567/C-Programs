44bcc9d916f0e8c4f2de4bdcff9224054120ee77
Raksha567 <rakshasawade2015@gmail.com>
2024-08-13 09:54:20 +0530

    Add files via upload

---
+++ CheckArrayOfIntegersContains5nextTo5.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void main(){
+	int n,i,found=0;
+	printf("Enter size of an array:");
+	scanf("%d",&n);
+	int a[n];
+	printf("Enter elements of an array:");
+	for(i=0;i<n;i++){
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++){
+		int num=a[i];
+		int pre_digit=-1;
+		while(num>0){
+			int current_digit=num%10;
+			num=num/10;
+			if(pre_digit==5&&current_digit==5){
+				found=1;
+				break;
+			}
+			pre_digit=current_digit;
+		}
+	}
+	printf("%d\n",found);
+	if(found==1){
+		printf("The array of integers contains 5 next to 5.");
+	}
+	else{
+		printf("Not found");
+	}
+}
