916adcdba97e343bbcb16375d5a375eb9d6e169a
Raksha567 <rakshasawade2015@gmail.com>
2024-08-14 12:13:16 +0530

    Update FindAbsoluteDifferenceBetweenNAnd51ifNisGreaterThan51tripleTheDifference.c

--- FindAbsoluteDifferenceBetweenNAnd51ifNisGreaterThan51tripleTheDifference.c
+++ FindAbsoluteDifferenceBetweenNAnd51ifNisGreaterThan51tripleTheDifference.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
+#include<stdlib.h>
 void main(){
 	int n,abDiff;
 	printf("Enter value of n");
 	scanf("%d",&n);
 	if(n>51){
-		abDiff=3*(n-51);
+		abDiff=3*abs(n-51);
 		printf("Result is  %d",abDiff);
 	}
 	else if(n<51){
-		abDiff=-(n-51);
+		abDiff=abs(n-51);
 		printf("Absolute difference between %d and 51 is %d",n,abDiff);
 	}
 }
