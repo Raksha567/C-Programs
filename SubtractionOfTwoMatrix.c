e776d9793c7d2857e33998828d024bf3e22379ec
Raksha567 <rakshasawade2015@gmail.com>
2024-08-14 13:53:38 +0530

    Add files via upload

---
+++ SubtractionOfTwoMatrix.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+void main(){
+	int a[2][2],b[2][2],c[2][2],i,j;
+	printf("Enter elements of first Matrix:");
+	for(i=0;i<2;i++){
+		for(j=0;j<2;j++){
+			scanf("%d",&a[i][j]);
+		}
+	}
+	printf("First matrix is:\n");
+	for(i=0;i<2;i++){
+		for(j=0;j<2;j++){
+			printf("%d ",a[i][j]);
+		}
+		printf("\n");
+	}
+	printf("Enter elements of second Matrix:");
+	for(i=0;i<2;i++){
+		for(j=0;j<2;j++){
+			scanf("%d",&b[i][j]);
+		}
+	}
+	printf("Second matrix is:\n");
+	for(i=0;i<2;i++){
+		for(j=0;j<2;j++){
+			printf("%d ",a[i][j]);
+		}
+		printf("\n");
+	}
+	printf("Subtraction of Matrix is:\n");
+	for(i=0;i<2;i++){
+		for(j=0;j<2;j++){
+			c[i][j]=a[i][j]-b[i][j];
+			printf("%d ",c[i][j]);
+		}
+		printf("\n");
+	}
+}
