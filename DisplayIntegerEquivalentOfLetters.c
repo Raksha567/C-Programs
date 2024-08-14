15bb3c786450ee8bbc45ad2c83b4fe62c72cbea9
Raksha567 <rakshasawade2015@gmail.com>
2024-08-14 12:16:05 +0530

    Update DisplayIntegerEquivalentOfLetters.c

--- DisplayIntegerEquivalentOfLetters.c
+++ DisplayIntegerEquivalentOfLetters.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 void main(){
 	char ch;
-	printf("Ascii Value of Lowercase Letters:\n");   
+	printf("Integer Value of Lowercase Letters:\n");   
 	for(ch='a';ch<='z';ch++){
-	printf("Ascii value of %c is %d\n",ch,ch);
+	printf("Integer value of %c is %d\n",ch,ch);
 }
-printf("Ascii value of Upper case letters:\n");
+printf("Integer value of Upper case letters:\n");
 for(ch='A';ch<='Z';ch++){
-	printf("Ascii value of %c is %d\n",ch,ch);
+	printf("Integer value of %c is %d\n",ch,ch);
 }
 	
 	
