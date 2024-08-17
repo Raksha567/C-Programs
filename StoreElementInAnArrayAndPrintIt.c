58494f0b73f81392c5776097b4a0767505f9df9e
Raksha567 <rakshasawade2015@gmail.com>
2024-08-17 18:12:01 +0530

    Add files via upload

---
+++ StoreElementInAnArrayAndPrintIt.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main(){
+	int n,i;
+	printf("Enter size of an array:");
+	scanf("%d",&n);
+	int a[n];
+	printf("Enter elements of an array:");
+	for(i=0;i<n;i++){
+		scanf("%d",&a[i]);
+	}
+	printf("Elements of an Array are:");
+	for(i=0;i<n;i++){
+		printf("%d ",a[i]);
+	}
+}
