5573120c79cfd06b5250f159c9fa7588bf13cdf7
Raksha567 <rakshasawade2015@gmail.com>
2024-08-19 14:43:04 +0530

    Update CheckaValueAppearsThreeTimesInanArray.c

--- CheckaValueAppearsThreeTimesInanArray.c
+++ CheckaValueAppearsThreeTimesInanArray.c
@@ -3,10 +3,17 @@ void main(){
 	int a[]={3,6,6,6,9,10};
 	int n=sizeof(a)/sizeof(a[0]);
 	int i;
+	int triple=0
 	for(i=0;i<n-2;i++){
 		if(a[i]==a[i+1]&&a[i]==(a[i+2])){
-			printf("Yes A value appers three times.");
+			triple=1;
 		}
 	
 	}
+	if(triple==1){
+		printf("A value is apeears three times");
+			}
+	else{
+		printf("A value doesnot appears three times");
+			}
 }
