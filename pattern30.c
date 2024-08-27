085e6cde75870726a53e6e774b0464c187d8a5f3
Raksha567 <rakshasawade2015@gmail.com>
2024-08-27 15:17:23 +0530

    Add files via upload

---
+++ pattern30.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void main(){
+	int i,j;
+	char k='@';
+	for(i=1;i<=3;i++){
+	
+	k++;
+		for(j=1;j<=4;j++){
+			
+			if(i+j==4||i+j==6||i+j==7){
+				
+				printf("%c",k);
+			
+				
+			}
+			
+				
+				else{
+				printf(" ");
+				
+				
+				
+			}
+			
+			
+		
+				
+			}
+		printf("\n");
+			
+		}
+		
+		
+	}
