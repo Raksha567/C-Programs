d00f17625c1b8b8ca334f670cdf4ca43869c0f6e
Raksha567 <rakshasawade2015@gmail.com>
2024-08-28 10:21:39 +0530

    Add files via upload

--- StoreNElementsInAnArrayAndPrintElementsUsingpointers.c
+++ StoreNElementsInAnArrayAndPrintElementsUsingpointers.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
 void main(){
 	int n,i;
-	printf("Size of an Element:");
+	printf("Size of an Array:");
 	scanf("%d",&n);
 	int a[n],*ptr;
-	
+	ptr=&a[0];
 	printf("Enter elements of an array:");
 	for(i=0;i<n;i++){
 		scanf("%d",&a[i]);
 	}
 	for(i=0;i<n;i++){
-		printf("%d ",*&a[i]);
+		printf("%d ",*ptr);
+		ptr++;
 	}
 	
 }
