e8fcbe4e7ffd9c1262edc77fa989c4ae76811b8b
Raksha567 <rakshasawade2015@gmail.com>
2024-08-29 13:02:53 +0530

    Update CheckWhetherSequenceOfNumbers1,2,3AppearsInAGivenArray.c

--- CheckWhetherSequenceOfNumbers1,2,3AppearsInAGivenArray.c
+++ CheckWhetherSequenceOfNumbers1,2,3AppearsInAGivenArray.c
@@ -2,19 +2,25 @@
 void main(){
 	int a[]={1,4,4,1,2,3,6,7,8};
 	int n=sizeof(a)/sizeof(a[0]);
-	int i;
+	int i,appears=0;
 	for( i=0;i<n;i++){
 		if(a[i]==1&&a[i+1]==2&&a[i+2]==3){
-			printf("True sequence of 1,2,3 is present in an array.");
+			appears=1;
 			
-			
-		
-		}
-
+			}
+	}
+	if(appears=1){
+		printf("True, Sequence of 1,2,3 is present in an array");
+	}
+	else{
+		printf("False,Sequence of 1,2,,3 is not present in an array ");
+	}
 		
 	}
 		
-		}
+	
+	
+	
 		
 	
 	
